5c06333883085cf1217e311fcd0eaf6ff48ea9e1
Hung <hungnguyen150300@gmail.com>
2022-03-30 07:09:17 +0300

    Linked db

--- config/database.js
+++ config/database.js
@@ -1,4 +1,4 @@
 module.exports = {
-  database: 'mongodb://localhost:27017/meanauth',
+  database: 'mongodb+srv://fullstack:hungbeo1503@cluster0.9jclw.mongodb.net/MEANauthapp?retryWrites=true&w=majority',
   secret: 'mysecret'
 }
